8ecf33edc8415420e43244596eefda97371ed8b9
Alexandra M. Hall <hallstrategygroup@gmail.com>
2022-05-24 16:47:02 -0400

    my-mobile-app

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Counter from './src/components/Counter';
+import Hero from './src/components/Hero';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Hero />
+      <Counter />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'gold',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import { Image } from 'react-native';
+
+export default function Hero() {
+return (
+    <Image 
+    style={{ width: 406, height: 232}}
+    source={{
+        uri: "https://www.computersciencedegreehub.com/wp-content/uploads/2016/02/what-is-coding-1024x683.jpg"
+    }} />
+)
+}
